9e3a8e1fbdcfde7a836a433281336511dc937d18
shhx <luisgomezlo.lg@gmail.com>
2025-10-03 01:43:55 +0200

    Refactor chart and map handling: improve GPS position update logic, enhance styling for responsiveness, and add toggle for auto-center feature.

--- data/main.js
+++ data/main.js
@@ -2,7 +2,7 @@ let combinedChart = null;
 
 function updateCombinedChart(rssi, dist) {
     const length = rssi.length;
-    const labels = Array(length).fill(0).map((_,i) => i - (length - 1));
+    const labels = Array(length).fill(0).map((_, i) => i - (length - 1));
 
     if (!combinedChart) {
         const ctx = document.getElementById('combinedChart').getContext('2d');
@@ -111,23 +111,53 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 let trackPoints = [];
-let trackLine = L.polyline(trackPoints, {color: 'blue'}).addTo(map);
-let marker = L.marker([0, 0]).addTo(map);
+let trackLine = L.polyline(trackPoints, { color: 'blue' }).addTo(map);
+let marker = L.marker()
 
+let auto_set_view = true;
 function updateGpsPos() {
     fetch('/current_pos')
-    .then(response => response.json())
-    .then(data => {
-        if (data.gps_fix_ok) {
-            let lat = data.gps_lat;
-            let lon = data.gps_lon;
-            trackPoints.push([lat, lon]);
-            trackLine.setLatLngs(trackPoints);
-            marker.setLatLng([lat, lon]);
-            map.setView([lat, lon], 15);
-        }
-    });
+        .then(response => response.json())
+        .then(data => {
+            if (data.gps_fix_ok) {
+                let lat = data.gps_lat;
+                let lon = data.gps_lon;
+                trackPoints.push([lat, lon]);
+                trackLine.setLatLngs(trackPoints);
+                marker.setLatLng([lat, lon]);
+                if (auto_set_view) {
+                    map.setView([lat, lon]);
+                }
+            }
+        });
 }
 
 setInterval(updateGpsPos, 1000);
 updateGpsPos();
+
+L.Control.Button = L.Control.extend({
+    options: {
+        position: 'topleft'
+    },
+    onAdd: function (map) {
+        var container = L.DomUtil.create('div', 'leaflet-bar leaflet-control');
+        var button = L.DomUtil.create('a', 'leaflet-control-button', container);
+        button.classList.add('active');
+        L.DomEvent.disableClickPropagation(button);
+        L.DomEvent.on(button, 'click', function () {
+            auto_set_view = !auto_set_view;
+            if (auto_set_view) {
+                button.classList.add('active');
+            } else {
+                button.classList.remove('active');
+            }
+        });
+
+        button.title = 'Toggle Auto-Center';
+        button.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-target" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><circle cx="12" cy="12" r="9" /><circle cx="12" cy="12" r="5" /><circle cx="12" cy="12" r="1" /></svg>';
+        return container;
+    },
+    onRemove: function (map) { },
+});
+var control = new L.Control.Button()
+control.addTo(map);
